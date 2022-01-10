100ab95f0d61dda7d80c984660c5951386869fe1
matovu-farid <matovu90@gmail.com>
2022-01-10 21:37:19 +0300

    Add date static

--- modules/date.js
+++ modules/date.js
@@ -1,10 +1,13 @@
 import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
+
+
 function actualTime() {
+  
   const now = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
   const time = document.querySelector('#date');
   time.innerHTML = now;
 }
-actualTime();
+//actualTime();
 
 setInterval(actualTime, [1000]);
---
+++ modules/static_date.js
@@ -0,0 +1,11 @@
+
+
+
+function actualTime() {
+    const time = document.querySelector('#date');
+    Date.now()
+    
+  time.innerHTML = (new Date()).toLocaleString("en-US")
+}
+actualTime();
+
