ec975422b1ad60e8a2ae9cd45688bb519216514a
matovu-farid <matovu90@gmail.com>
2022-01-10 15:17:13 +0300

    Refactor files to modules directory

---
+++ index.js
@@ -0,0 +1,12 @@
+import BookManager from './modules/book_manager.js';
+import DisplayBooks from './modules/display_books.js';
+import FormListener from './modules/form_listener.js';
+import Navigation from './modules/navigation.js';
+
+const bookManager = new BookManager();
+const displayBooks = new DisplayBooks(bookManager.bookCollection);
+displayBooks.display();
+const formListener = new FormListener(bookManager);
+formListener.addEventListener();
+const navigation = new Navigation();
+navigation.attachListeners();
